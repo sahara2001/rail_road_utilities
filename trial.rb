1a1355b9511d2a05f57cabead9f9b6a2ffbb349f
sahara2001 <sun201709@outlook.com>
2019-01-21 12:47:39 -0500

    use trial.rb to test your gosu build

---
+++ trial.rb
@@ -0,0 +1,22 @@
+require 'gosu'
+
+
+class Tutorial < Gosu::Window
+  def initialize
+    super(800,600)
+    self.caption = 'tutorial game'
+
+  end
+
+  def update
+    
+  end
+
+  def draw
+
+  end
+
+end
+
+window = Tutorial.new
+window.show
